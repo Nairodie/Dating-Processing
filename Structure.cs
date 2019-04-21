1a3957435c1459c09d51b3c59260890b4386672d
Nairodie <42880967+Nairodie@users.noreply.github.com>
2019-04-21 17:25:03 -0500

    Update Structure.cs

--- Structure.cs
+++ Structure.cs
@@ -8,3 +8,21 @@ enum AccountState
   Frozen,
   CLosed
 };
+
+struct Account
+	{
+		public AccountState State;
+		public string Name;
+		public string Address;
+		public int AccountNumber;
+		public int Balance;
+		public int Overdraft;		
+	};
+
+class BankProgram
+{
+  public static void Main()
+  {
+    
+  }
+}
